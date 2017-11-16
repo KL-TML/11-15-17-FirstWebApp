90c319182ad97e72073f058475a4194c8971cd1d
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-15 21:55:47 -0500

    second commit

--- server.rb
+++ server.rb
@@ -9,5 +9,20 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = [ 'git', 'HTML', 'CSS', 'Ruby']
+  @interest = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_info
+end
+
+get '/favourites' do
+  @fav_links = ['imdb.com', 'espn.com', 'whales.com', 'bitmaker.com']
+  erb :fav_links_to
+end
+
+# get '/' do
+#   redirect to('/home')
+# end
+
+get '/gallery' do
+  redirect to('/portfolio')
 end
